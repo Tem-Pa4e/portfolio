9a272d1dc142f1bbd95c991231923f511d392358
Sasha Chumasov <sashachumasov@yahoo.com>
2022-05-15 19:02:39 +0300

    refactoring

--- src/component/content/portfolioPage/PortfolioPage.js
+++ src/component/content/portfolioPage/PortfolioPage.js
@@ -2,22 +2,38 @@ import React from 'react';
 import st from './PortfolioPage.module.css'
 
 export const PortfolioPage = () => {
+
+
+
+
     return (
         <div className={st.portfolioContainer}>
             <div className={st.headerPortfolio}>
                 <div>
-                    <h5>Minimal Prtfolio</h5>
+                    <h3>My Works</h3>
                 </div>
 
                 <ul className={st.listOfWorks}>
-                    <li>All</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
+                    <li style={{marginLeft: '0'}}>All</li>
+                    <li>React/JS</li>
+                    <li>React/TS</li>
                     <li>Angular</li>
                 </ul>
             </div>
-            <div className={st.listWrapper}>
+            <div  className={st.listWrapper}>
                 <ul>
+                        <li>
+                            <div>
+                                <div>
+                                    <img src="http://tokyo.ibthemespro.com/assets/img/portfolio/4.jpg" alt=""/>
+                                </div>
+                                <div className={st.descriptionListWrapper}>
+                                    <h5>todolist</h5>
+                                    <span>react</span>
+                                </div>
+                            </div>
+                        </li>
+
                     <li>
                         <div>
                             <div>
@@ -46,8 +62,8 @@ export const PortfolioPage = () => {
                                 <img src="http://tokyo.ibthemespro.com/assets/img/portfolio/4.jpg" alt=""/>
                             </div>
                             <div className={st.descriptionListWrapper}>
-                                <h5>todolist</h5>
-                                <span>react</span>
+                                    <h5>todolist</h5>
+                                    <span>react</span>
                             </div>
                         </div>
 
@@ -64,12 +80,12 @@ export const PortfolioPage = () => {
                         </div>
                     </li>
 
-        </ul>
-</div>
+                </ul>
+            </div>
 
 
-</div>
-)
-    ;
+        </div>
+    )
+        ;
 };
 
